37ceeccfda2cd9df6e9da860b2eddbf82a93462b
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-04-20 09:13:37 -0300

    Update GitHub Workflow Seg 20 Abr 2020 09:13:36 -03

---
+++ automacao-commits.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# chmod 744 -> Owner Group Public -> -rwxr--r--
+# ARRAYS: https://linuxconfig.org/how-to-use-arrays-in-bash-script
+
+files_add=(
+    # .gitignore
+    anotacoes.txt
+    arduino-esp8266.Dockerfile
+    automacao-commits.sh
+    platformio.Dockerfile
+    testes.Dockerfile
+)
+
+commit_msg="Update GitHub Workflow $(date)"
+branch_name="master"
+repo_name="origin-testes"
+
+for file in "${files_add[@]}";
+do
+    echo "git add $file"
+    git add $file
+done
+
+echo "git commit -m \"$commit_msg\""
+git commit -m "$commit_msg"
+
+# echo "git push $repo_name $branch_name"
+# git push $repo_name $branch_name
