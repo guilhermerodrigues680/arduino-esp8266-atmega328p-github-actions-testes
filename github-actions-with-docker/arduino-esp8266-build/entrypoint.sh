5619891ee38a9909da7cbefafe10819a778da2bb
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-04-21 16:17:05 -0300

    Update GitHub Workflow Ter 21 Abr 2020 16:17:05 -03

--- github-actions-with-docker/arduino-esp8266-build/entrypoint.sh
+++ github-actions-with-docker/arduino-esp8266-build/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Set Sketchbook Default Location
-# cd /root/Arduino
+cd /root/Arduino
 
 echo "\$0 = $0"
 echo "\$1 = $1"
 echo "\$2 = $2"
 echo "\$3 = $3"
 
-
 time=$(date)
 echo "::set-output name=time::$time"
+
+# repository_github="$1"
+# path_ino_file="$2"
+# ino_file="$3"
+
+git clone "https://github.com/$1.git" repository
+cd "repository/$2"
+
+arduino -v --board esp8266com:esp8266:nodemcu --verify $3
